01efa01e3c8bcf4fd0e4013ea70043afbaa85fa8
Christopher David <chris@openagents.com>
2024-12-12 15:19:28 -0600

    card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,47 @@
+import * as React from "react"
+import styles from "@components/Card.module.scss"
+
+interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+  children?: React.ReactNode;
+  title?: string | any;
+  mode?: string | any;
+}
+
+const Card: React.FC<CardProps> = ({ children, mode, title, ...rest }) => {
+  let titleElement = (
+    <header className={styles.action}>
+      <div className={styles.left} aria-hidden="true"></div>
+      <h2 className={styles.title}>{title}</h2>
+      <div className={styles.right} aria-hidden="true"></div>
+    </header>
+  );
+
+  if (mode === 'left') {
+    titleElement = (
+      <header className={styles.action}>
+        <div className={styles.leftCorner} aria-hidden="true"></div>
+        <h2 className={styles.title}>{title}</h2>
+        <div className={styles.right} aria-hidden="true"></div>
+      </header>
+    );
+  }
+
+  if (mode === 'right') {
+    titleElement = (
+      <header className={styles.action}>
+        <div className={styles.left} aria-hidden="true"></div>
+        <h2 className={styles.title}>{title}</h2>
+        <div className={styles.rightCorner} aria-hidden="true"></div>
+      </header>
+    );
+  }
+
+  return (
+    <article className={styles.card}>
+      {titleElement}
+      <section className={styles.children}>{children}</section>
+    </article>
+  );
+};
+
+export default Card;
