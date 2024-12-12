4ea253e242b13c4db580bf960ca4b82b6fc2ea3c
Christopher David <chris@openagents.com>
2024-12-12 15:39:24 -0600

    ya

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import styles from "@components/Card.module.scss"
+import styles from "./Card.module.scss"
 
 interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
