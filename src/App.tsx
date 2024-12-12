c12443445a1b6318c7503af5742a34a54ccd4924
Christopher David <chris@openagents.com>
2024-12-12 15:42:29 -0600

    Ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css"
+import "./styles/global.scss"
 import { useState } from "react"
 import { invoke } from "@tauri-apps/api/core"
-import reactLogo from "./assets/react.svg"
 import Card from "./components/Card"
 
 function App() {
@@ -20,19 +20,6 @@ function App() {
       <Card title="Left" mode="left">
         <p>Card with title on the left.</p>
       </Card>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
         className="row"
