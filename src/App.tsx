89bc3405db65543654c72aa326ddf616ef408ed8
Christopher David <chris@openagents.com>
2024-12-12 19:40:26 -0600

    clean up app

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,13 @@
 import "./App.css"
 import "./styles/global.scss"
-import { useState } from "react"
-import { invoke } from "@tauri-apps/api/core"
 import Card from "./components/Card"
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
     <main className="container">
-      <h1>Pylon</h1>
-
-      <Card title="Welcome" mode="left">
-        <p>We are going to do cool stuff.</p>
+      <Card title="Welcome to Pylon">
+        <p>Awaiting connection</p>
       </Card>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-      <p>{greetMsg}</p>
     </main>
   );
 }
